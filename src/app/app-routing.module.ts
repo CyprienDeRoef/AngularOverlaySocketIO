3d94575735eca2b68c1f7e77afb2f2721706cb34
CyprienDeRoef <c.deroef@outlook.fr>
2019-09-06 18:10:51 +0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InterfaceComponent } from './interface/interface.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { StartingComponent } from './starting/starting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InterfaceComponent },
+  { path: 'interface', component: InterfaceComponent },
+  {path: 'overlay', component: OverlayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterfaceComponent } from './interface/interface.component';
+import { OverlayComponent } from './overlay/overlay.component';
+import { StartingComponent } from './starting/starting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfaceComponent,
+    OverlayComponent,
+    StartingComponent
   ],
   imports: [
     BrowserModule,
